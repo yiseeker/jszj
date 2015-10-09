3cc42cc43ad53f55c674ccf4d63c080bbb22b89d
linux <linux@MACHINE.(none)>
2015-10-09 17:00:22 +0800

    2015-10-09

--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var formidable=require('formidable');
 var fs =require('fs');
-
+var uuid=require('node-uuid');
 
 
 /* 上传 */
@@ -10,8 +10,22 @@ router.post('/', function(req, res,next) {
     var form=new formidable.IncomingForm();
     form.parse(req,function(err,fields,files){
         try{
-            fs.renameSync(files.upload.path,'/home/linux/Projects/jszj/upload/'+files.upload.name);//这里的保存路径是系统的路径，不是站点的路径
-            res.send("<script>window.parent.CKEDITOR.tools.callFunction("+req.query.CKEditorFuncNum+",'/resource/"+files.upload.name+"','')</script>");
+            var fileType;
+            for(var i in files)
+            {
+                if(files[i].type!='image/jpeg' && files[i].type!='image/png' && files[i].type!='image/gif' && files[i].type!='image/bmp' && files[i].type!='image/jpg')
+                {
+                    throw new Error('上传的不是图片类型的文件!');
+                }
+                else
+                {
+                   fileType=files[i].type.split('/')[1];
+                }
+
+            }
+            var fileName=req.user.username+'_'+uuid.v4()+'.'+fileType;
+            fs.renameSync(files.upload.path,'/home/linux/Projects/jszj/upload/'+fileName);//这里的保存路径是系统的路径，不是站点的路径
+            res.send("<script>window.parent.CKEDITOR.tools.callFunction("+req.query.CKEditorFuncNum+",'/resource/"+fileName+"','')</script>");
         }catch(e)
         {
             res.send('上传失败,原因:\n'+e);
